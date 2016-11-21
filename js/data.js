34123f3c31f938f3f31e743e9d465748b2de0dcf
Scott Schermerhorn <plaidscott@gmail.com>
2016-11-21 13:16:20 -0700

    initial setup
    basic structure for project, all files and folders created. files
    linked, firebase linked. sample data created in data.js folder

---
+++ js/data.js
@@ -0,0 +1,90 @@
+var housing = {
+    provo: {
+        roomOne: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomTwo: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomThree: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomFour: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomFive: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomSix: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomSeven: {
+        occupant: "Scott Schermerhorn",
+        phone: "801-857-7898",
+        email: "plaidscott@gmail.com",
+        slackHandle: "plaidscott"
+    },
+    orem: {
+        roomOne: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomTwo: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomThree: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomFour: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomFive: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomSix: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        },
+        roomSeven: {
+            occupant: "Scott Schermerhorn",
+            phone: "801-857-7898",
+            email: "plaidscott@gmail.com",
+            slackHandle: "plaidscott"
+        }
+
+    }
+};
